154f1e5cdc9832f2c416352c2688470f9cedeb68
Nathan Y. C <50110608+nathanychin@users.noreply.github.com>
2021-11-08 03:53:34 -0500

    Set API link
    Set API link and input form

---
+++ react-crypto-tracker/README.md
@@ -0,0 +1,5 @@
+API Generated from https://www.coingecko.com/en/api/documentation
+Using https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false
+
+
+Using React and Axios
--- react-crypto-tracker/src/App.js
+++ react-crypto-tracker/src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  <div>API</div>
+  
+  // coins is the value, setCoins is the function to use on the value
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+    }).catch(() => alert('Error with API link'))
+  }, [])
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search for currencies</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input" />
+        </form>
+      </div>
+      
+    </div>
+  )
 }
 
 export default App;
